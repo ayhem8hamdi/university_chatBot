0ce811463de90dc2a3181bc2c997918a8b67cca7
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-03-02 15:19:20 +0100

    Create Utils Folder & App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color primaryColor = const Color(0xff0114C6);
+  static Color secondaryColor = const Color(0xffEAF4FF);
+  static Color logoGreenColor = const Color(0xff007B1C);
+  static Color surfaceColor = const Color(0xffF8F9FA);
+  static Color gray1Color = const Color(0xff8C8C8C);
+  static Color gray2Color = const Color(0xffD2D2D2);
+  static Color gray3Color = const Color(0xffAEAEAE);
+  static Color textColor = const Color(0xff2C2C2C);
+}
