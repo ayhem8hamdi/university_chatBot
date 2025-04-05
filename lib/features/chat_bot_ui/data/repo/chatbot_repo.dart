4936cd0785bc26e17f5f096c44df1da0a86e4208
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-04-05 06:42:42 +0100

    Add chatbot repo

--- lib/features/chat_bot_ui/data/repo/chatbot_repo.dart
+++ lib/features/chat_bot_ui/data/repo/chatbot_repo.dart
@@ -1,1 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:university_chatbot/core/utils/failure/failure.dart';
+import 'package:university_chatbot/features/chat_bot_ui/data/model/chatbotmodel.dart';
 
+abstract class ChatbotRepo {
+  Future<Either<Failure, ChatbotModel>> getChatbotResponse(String userInput);
+}
